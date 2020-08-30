0c8f29452d046f10c4c736df47efca787a872145
Juan Pablo Rabino <juan@pop-os.localdomain>
2020-08-30 14:35:41 -0300

    adding filters to the data queryset Fixes #9

--- vila/ambiente/views.py
+++ vila/ambiente/views.py
@@ -46,9 +46,23 @@ class DeviceDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = DeviceSerializer
     
 class DeviceDataView(generics.ListCreateAPIView):
-    queryset = DeviceData.objects.all()
     serializer_class = DeviceDataSerializer
-    
+    def get_queryset(self):
+        """
+        Optionally restricts the returned device to a determined period
+        """
+        queryset = DeviceData.objects.all()
+        from_dt = self.request.query_params.get('from_dt', None)
+        to_dt = self.request.query_params.get('to_dt', None)
+        device_id = self.request.query_params.get('device_id', None)
+        if device_id is not None:
+            queryset = queryset.filter(device_id=device_id)
+        if from_dt is not None:
+            queryset = queryset.filter(timestamp__gte=from_dt)
+        if to_dt is not None:
+            queryset = queryset.filter(timestamp__lte=to_dt)
+        return queryset
+
     def post(self, request, *args, **kwargs):
         device_inst,create=Device.objects.get_or_create(device_id = request.data.get('device_id','0'))
         
